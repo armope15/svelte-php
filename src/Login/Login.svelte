dba40d1dba262eabbb950984919794ff23e3805f
Armope <armope@tutanota.com>
2020-04-24 02:48:34 +0200

    InputCustom
    Añadido el input custon, que añade un icono desde google icons.

--- src/Login/Login.svelte
+++ src/Login/Login.svelte
@@ -1,1 +1,32 @@
+<script>
+    import InputCustom from '../Componentes/InputCustom.svelte'
+
+
+    function login(){}
+
+</script>
+
 <h1>Login</h1>
+
+<form on:submit|preventDefault={login} id="loginForm" autocomplete="off">
+
+    <InputCustom 
+        type="email"
+        id="email"
+        name="email"
+        label="email"
+        icon="account_circle"
+    />
+
+     <InputCustom 
+        type="password"
+        id="pass"
+        name="pass"
+        label="password"
+        icon="https"
+    />
+
+   
+
+
+</form>
